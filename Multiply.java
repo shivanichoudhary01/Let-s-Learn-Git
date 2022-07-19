2d9888cfd45d26a4c95fd5efccc90e56464eb723
Shivani Choudhary <abc@gmail.com>
2022-07-19 16:05:53 -0700

    Added Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,11 @@
+package Letslearngit;
+
+public class Multiply {
+  
+        public static void main(String[] args){
+            int a=5;
+            int b=6;
+            System.out.print(a*b);
+        }
+    
+}
