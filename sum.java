24fd57a04fe9ca74d8d7faad58c57c1971acecbe
Shivani Choudhary <abc@gmail.com>
2022-07-19 15:49:26 -0700

    added message in sum.java file

--- sum.java
+++ sum.java
@@ -4,6 +4,6 @@ public class sum{
     public static void main(String[] args){
         int a=5;
         int b=6;
-        System.out.print(a+b);
+        System.out.print("The sum is:-"+ a+b);
     }
 }
